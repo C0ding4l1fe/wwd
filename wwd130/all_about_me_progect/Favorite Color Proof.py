35746e2e92b8911c57273c41cec60ea4204861ab
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-09-26 16:26:46 -0700

    fgh

--- wwd130/all_about_me_progect/Favorite Color Proof.py
+++
@@ -1,6 +0,0 @@
-color=input("Hey there! What's your favorite color?")
-print('Great Choice! '+color+ ' is an awesome color!')
-if color=="red": print('My favorite color is red too!') 
-elif color=="Red": print('My favorite color is red too!')
-else: print('My favorite color is red.')
-
