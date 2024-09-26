f5abc8963c2add20415832625b8775e055f34958
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-09-25 20:11:48 -0700

    initial commit

---
+++ all_about_me_progect/Bond, James Bond.py
@@ -0,0 +1,3 @@
+first_name=input("What's your first name?")
+last_name=input ("And what's your last name again?")
+input ("Ah, so your name is "+last_name+", "+first_name+" "+last_name+"!")
---
+++ all_about_me_progect/Favorite Color Proof.py
@@ -0,0 +1,6 @@
+color=input("Hey there! What's your favorite color?")
+print('Great Choice! '+color+ ' is an awesome color!')
+if color=="red": print('My favorite color is red too!') 
+elif color=="Red": print('My favorite color is red too!')
+else: print('My favorite color is red.')
+
