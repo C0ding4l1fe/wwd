073110e28ec81edf418b94664845a2af66346ed3
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-10-14 16:41:50 -0700

    Checkpoint Accomplished

---
+++ wwr/styles/Conditions Checkpoints.py
@@ -0,0 +1,13 @@
+firstnumber=input("What is your first number?:")
+secondnumber=input("What is your second number?:")
+if firstnumber >secondnumber: 
+    print("The first number is greater than the second!")
+else: print("The first number is not greater than the second!")
+if firstnumber==secondnumber: print("The numbers are equal!")
+else: print("The numbers are not equal!")
+if secondnumber>firstnumber: print("The second number is greater than the first!")
+else: print("The second number is not greater than the first!")
+animal=input("What's your favorite animal?")
+if animal.lower()=="tazmanian tiger":
+    print("No way! Tazmanian Tigers are my favorite animals too!")
+else: print("That's not my favorite, but its a good one!")
