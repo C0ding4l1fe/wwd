a437567f3d95344e8e0f02fd4f2a98443c0a3a7d
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-10-18 10:08:00 -0700

    Story Additions!

--- wwr/Adventure Game Milestone.py
+++ wwr/Adventure Game Milestone.py
@@ -9,7 +9,8 @@ choiceright=""
 pet=""
 drink=""
 tackle=""
-
+eating=""
+follow=""
 
 
 choiceone=input("You wake up feeling very groggy, with a splitting headache and blurry vision. As it clears, you find yourself within a dimly lit mansion. Its the middle of the night, and you hear sounds of a storm outside. The mansion is eerily silent, with only a dozen candles to light your view. You seem to be in the entry room of the mansion, with a staircase leading to a second floor, a door to the right and left, and a set of double doors on the far wall. Do you go up the STAIRS, to the LEFT, or to the RIGHT?")
@@ -25,26 +26,72 @@ elif choiceone.lower() in("leave","double doors","doors"):
     end=input("Click Enter to End the Program")
     if end=="": quit()
     else: quit()
-    #Second Choice Responses
+#Stairs Choice Responses
 if choicestairs.lower()=="run":
     choicerun=input("Hassen")
 elif choicestairs.lower()=="fight":
     choicefight=input("Pie")
 elif choicestairs.lower()=="evade":
     choiceevade=input("Racked")
-if choiceleft.lower()=="grab":
-            choicegrab=input('You grab some food, and run as fast as you can, as you do, you hear a louder sound behind you, and a deep voice shout "PUT THAT BACK!". Do you TURN around, keep RUNNING, or EAT the food as fast as you can?')
-if choicegrab.lower()=="eat":
-      choiceeat=input("Strudel")
-if choicegrab.lower()=="turn":
-      print("Hello")
-if choicegrab.lower()=="running": 
-      runningaway=input("Hi")
 
+#Left Choice Responses
 elif choiceleft.lower()=="look":
-    choicelook=input("sadf")
+    choicelook=input("You look under the table to see a woman in armor, climbing down a pit under the table. She doesn't notice you as he leaves to who knows where. Do you FOLLOW her, or IGNORE her?")
+    if choicelook.lower()=="ignore":
+        input("You ignore the woman, lower the tablecloth, and continue to eat contentedly, as you do, you begin to feel even more tired, and fall asleep on the table. When you wake, you are no longer in the dining room, and are tied to a chair. A very angry man is sitting in front of you. He berates you for eating his food, and asks you for an explanation. Do you LIE, or tell the TRUTH?")
+    if eating.lower()=="truth":
+        input("You decide to tell the truth. You explain how you don't know how you got here, and just want to get home. You were hungry, and saw the abandoned food and figured it wouldn't hurt anything to eat some of it. The man's face shifts from anger to pity. He frees you from the chair, and says he'll help you get home. He leads you out of the mansion, where a team of horses are waiting, attached to a carriage. You give him the directions to your house, and in about 15 minutes, you are safe at home. You thank the man as he speeds away, and he smiles and waves goodbye, speeding into the night. You still don't know how you got in the mansion, and rightfully you don't want to. You're just glad to be home. You've WON!")
+        end=input("Click Enter to End the Program")
+        if end=="": quit()
+        else: quit()
+    if eating.lower()=="lie":
+        input('You decide to lie. You say you thought the food was for you, and that you are insulted by his accusations. You try to pull the "Do you know who I am!?" card, but nothing seems to be working. The man seems to get angrier with every word you say, until eventually, he silences you, and says with a snarl, "I am the lord of this castle, and you will stay here, until you learn respect, you vile thief!" The man stood up, walks behind you. You hear a thud and a locking mechanism. He seems to have left you trapped in here, with no means of escape. I'"'m sorry, you've LOST!")
+        end=input("Click Enter to End the Program")
+        if end=="": quit()
+        else: quit()")
+    
+    
+    
+    
+    
+    if choicelook.lower()=="follow":
+        follow=input("You decide to follow her down the pit, climbing down the ladder a short while after she's gone. You reach the bottom, and the woman is gone. You find yourself in an underground tunnel, do you go FORWARD, or BACKWARD from the ladder?")
+    if follow.lower()=="forward":
+        forward=input("You decide to move forward, travelling down the tunnel for a long time, passing by many offshoots and tunnels. The tunnel is very dimly lit, lit only by the occasional torch. After a while, you begin to feel lost. You've past far beyond what you think the grounds of the mansion should be, but haven't seen any sign of escape. Just as you begin to give up hope, you see some natural light shining down from a patch of the ceiling. Eagerly, you run forward, and find a ladder, leading up to a sewer grate. you climb the ladder, push with all your might, and manage to remove the grate. You climb out of the tunnel, replace the grate, and look around. You recognize this place! Your in a field not far from your home! You begin to walk towards home, grateful to be safe and sound." '"Who knows what happened to that lady," you whisper to yourself as you walk, but you stop worrying about it after a while. If your lucky, you won'"'t have to worry about anything that happened tonight for a long time. You've WON!")
+        end=input("Click Enter to End the Program")
+        if end=="": quit()
+        else: quit()
+    if follow.lower()=="backward":
+        backward=input("You decide to move backward from the ladder, and notice yourself going deeper and deeper farther down. You're not sure if that's the right way to go, but continue regardless. You also feel water beginning to gather on the floor of the tunnel, the water level getting higher as you continue. After about an hour of walking, you notice the torches lighting the walls are out. Cautiously you move forward even further, but you slip, as you do you begin to slide along the floor, and the water begins to flow downward even further. Caught in the current, you descend faster and faster into the tunnel. You can barely see anything, but you hear a large crunching sound, like the clashing of large teeth. as the sound gets louder and louder, you get more and more frightened. Your surroundings get darker suddenly, before you hear a final CRUNCH! You've been eaten, and YOU'VE LOST!")
+        end=input("Click Enter to End the Program")
+        if end=="": quit()
+        else: quit()
+elif choiceleft.lower()=="grab":
+    choicegrab=input('You grab some food, and run as fast as you can, as you do, you hear a louder sound behind you, and a deep voice shout "PUT THAT BACK!". Do you TURN around, keep RUNNING, or EAT the food as fast as you can?')
+    if choicegrab.lower()=="eat":
+      choiceeat=input("You eat what you grabbed, and feel very strange. you begin to feel cold, as you look down to see your own body turning to ice. Whatever that food was, its freezing you alive. You have only enough time to scream before becoming a human popsicle. YOU'VE LOST!")
+      end=input("Click Enter to End the Program")
+      if end=="": quit()
+      else: quit()
+    if choicegrab.lower()=="turn":
+      print("Hello")
+    if choicegrab.lower()=="running": 
+      runningaway=input("You continue to run, ignoring whatever had called out to you. You manage to make it to the doorway, but as you cross the threshold you are hit in the back by something, hard. As you stand back up, you see your feet turn to stone. The stone creeps higher and higher, until your nothing but a statue, standing in the doorway. sorry, but YOU'VE LOST!")
+      end=input("Click Enter to End the Program")
+      if end=="": quit()
+      else: quit()
 elif choiceleft.lower()=="eating":
-    choiceeating=input("C")
+    eating=input("You continue to eat, and as you do, you never seem to get full. You just eat, and eat, and eat, until eventually, you're too tired to continue, and fall asleep on the table. When you wake, you are no longer in the dining room, and are tied to a chair. A very angry man is sitting in front of you. He berates you for eating his food, and asks you for an explanation. Do you LIE, or tell the TRUTH?")
+    if eating.lower()=="truth":
+        input("You decide to tell the truth. You explain how you don't know how you got here, and just want to get home. You were hungry, and saw the abandoned food and figured it wouldn't hurt anything to eat some of it. The man's face shifts from anger to pity. He frees you from the chair, and says he'll help you get home. He leads you out of the mansion, where a team of horses are waiting, attached to a carriage. You give him the directions to your house, and in about 15 minutes, you are safe at home. You thank the man as he speeds away, and he smiles and waves goodbye, speeding into the night. You still don't know how you got in the mansion, and rightfully you don't want to. You're just glad to be home. You've WON!")
+        end=input("Click Enter to End the Program")
+        if end=="": quit()
+        else: quit()
+    if eating.lower()=="lie":
+        input('You decide to lie. You say you thought the food was for you, and that you are insulted by his accusations. You try to pull the "Do you know who I am!?" card, but nothing seems to be working. The man seems to get angrier with every word you say, until eventually, he silences you, and says with a snarl, "I am the lord of this castle, and you will stay here, until you learn respect, you vile thief!" The man stood up, walks behind you. You hear a thud and a locking mechanism. He seems to have left you trapped in here, with no means of escape. I'"'m sorry, you've LOST!")
+        end=input("Click Enter to End the Program")
+        if end=="": quit()
+        else: quit()
 
 
 #Right Path Choices
