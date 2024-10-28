66a495f18e0b2f53744fa532759330df2d786730
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-10-28 08:09:12 -0700

    while loop edit

--- While.py
+++ While.py
@@ -1,5 +1,14 @@
-number=int(input("Please enter a positive number!"))
+number=float(input("Please enter a positive number!"))
 while number <0:
     print("I'm sorry, but that's a negative number, I asked for a positive one. Could you please enter a positive number?")
-    number=int(input("Please enter a positive number!"))
-print("The number is {number}")
+    number=float(input("Please enter a positive number!"))
+
+print(f"The number is {number}!")
+candy=input("Can I have some candy?")
+while candy.lower()=="no":
+    candy=input("Can I have some candy?")
+while candy.lower()=="yes":
+    print("Thank You!")
+    end=input("Press Enter to End the Program")
+    if end=="":
+        quit()
