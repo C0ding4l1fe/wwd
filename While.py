d86b1176e75ca0468c0333226caaf72d778f57c0
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-10-28 09:13:35 -0700

    counter code

--- While.py
+++ While.py
@@ -1,7 +1,11 @@
 number=float(input("Please enter a positive number!"))
+count = 0
 while number <0:
-    print("I'm sorry, but that's a negative number, I asked for a positive one. Could you please enter a positive number?")
-    number=float(input("Please enter a positive number!"))
+        print("I'm sorry, but that's a negative number, I asked for a positive one. Could you please enter a positive number?")
+        number=float(input("Please enter a positive number!"))
+        count+=1
+print(f"You tried {count+1} times!")
+
 
 print(f"The number is {number}!")
 candy=input("Can I have some candy?")
