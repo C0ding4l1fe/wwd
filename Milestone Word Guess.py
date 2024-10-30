164cb29864ec06ca7d03ba1c7db247a6a16a8344
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-10-29 18:38:14 -0700

    milestone complete code

---
+++ Milestone Word Guess.py
@@ -0,0 +1,13 @@
+underscores="_ _ _ _ _"
+print("Welcome to the guessing game!")
+
+guess=input("Please make your guess!:"+underscores_used)
+count = 0
+while not guess.lower() =="alma":
+        print("I'm sorry, but that's incorrect, please guess again!")
+        guess=input("Please make your guess!:"+underscores)
+
+        count+=1
+print(f"Congradulations, you got it! You tried {count+1} times!")
+
+
