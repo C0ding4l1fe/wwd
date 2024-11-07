0490aa86da378fca1286f534245ff5c84d4d558a
C0ding4l1fe <cir4bus4ev3r@byui.edu>
2024-11-07 12:28:51 -0700

    fdsa

--- wwr/Commitment team activity.py
+++
@@ -1,6 +0,0 @@
-capital=input("What's your favorite letter in the word commitment?")
-commitment="commitment"
-for letters in commitment:
-  if letters==capital.lower():
-    letters="_"
-  print(letters,end="")
